932da3a1c31a76ff91402620c0dd38fc885a2d63
guominfang <gmf0927@163.com>
2019-06-20 16:25:09 +0800

    【开发】添加修改工程名类文件（未实现功能）

---
+++ OCMixtool/MixProjectName/OCMProjectName.h
@@ -0,0 +1,19 @@
+//
+//  OCMProjectName.h
+//  OCMixtool
+//
+//  Created by guominfang on 2019/6/20.
+//  Copyright © 2019年 Kowk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OCMProjectName : NSObject
+
++ (void)modityProjectName;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ OCMixtool/MixProjectName/OCMProjectName.m
@@ -0,0 +1,17 @@
+//
+//  OCMProjectName.m
+//  OCMixtool
+//
+//  Created by guominfang on 2019/6/20.
+//  Copyright © 2019年 Kowk. All rights reserved.
+//
+
+#import "OCMProjectName.h"
+
+@implementation OCMProjectName
+
++ (void)modityProjectName {
+    
+}
+
+@end
